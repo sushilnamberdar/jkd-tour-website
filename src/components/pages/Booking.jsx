4e20eb9c83a9951526af4ac9d7b96b79a697b8ff
sushilnamberdar <sushilnamberdar000@gmail.com>
2026-02-10 11:24:02 +0530

    update in booking ui

--- src/components/pages/Booking.jsx
+++ src/components/pages/Booking.jsx
@@ -32,7 +32,7 @@ export default function Booking() {
         setLoading(true);
 
         try {
-            const res = await fetch("https://api.jkdtours.com/book-tour.php", {
+            const res = await fetch("https://api.jkdtour.com/book-tour.php", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form),
